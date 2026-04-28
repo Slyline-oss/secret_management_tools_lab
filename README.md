a2214a388c972dfa131f6d0a2ceea0a63dde7f70
artjom <torino1337@gmail.com>
2026-04-28 17:30:01 +0300

    Testing secrets exposed

---
+++ README.md
@@ -0,0 +1,4 @@
+## Initial Setup
+
+GH token is (S5):
+GITHUB_TOKEN=ghp_1234exampleoftoken
---
+++ app/SecretsMngLab/src/main/java/org/melihovs/secretsmnglab/config/SecretsConfig.java
@@ -0,0 +1,11 @@
+package org.melihovs.secretsmnglab.config;
+
+public class SecretsConfig {
+
+    // S3
+    public static final String JWT_SECRET = "jwt-secret-very-secure-123456";
+
+    // S7
+    public static final String INTERNAL_TOKEN = "some-token-abcdef-123456";
+
+}
